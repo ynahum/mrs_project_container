49955d19fd20c03902c32bfdd76a4f6d43575fd6
Yair Nahum <ynahum@gmail.com>
2025-03-30 22:31:15 +0000

    nav2 params fixes + launch file

--- run.sh
+++ run.sh
@@ -4,10 +4,13 @@ set -e
 source /home/dev/ros_ws/src/setup.sh
 
 # launch AutoDRIVE Devkit with GUI and Rviz
-ros2 launch my_car rviz_sim_bringup.launch.xml
+#ros2 launch my_car rviz_sim_bringup.launch.xml
 
 # also slam
 #ros2 launch my_car slam_rviz_sim_bringup.launch.xml
+
+# launch AutoDRIVE Devkit with GUI, nav2 and Rviz
+ros2 launch my_car rviz_nav2_sim_bringup.launch.xml
 
 # wall follow launch
 #ros2 launch my_car wf_slam_rviz_sim_bringup.launch.xml
