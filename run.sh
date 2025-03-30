8f08d1578ab3a69f78a93cd3f422fbfb0bacf4aa
Yair Nahum <ynahum@gmail.com>
2025-03-30 07:17:38 +0000

    rename launch files

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@ set -e
 
 source /home/dev/ros_ws/src/setup.sh
 
-# launch wall_follow which also launches AutoDRIVE Devkit with GUI and Rviz
-ros2 launch wall_follow sim_bridge_plus_rviz_plus_slam.launch.xml
+# launch AutoDRIVE Devkit with GUI and Rviz
+ros2 launch my_car sim_bringup_rviz.launch.xml
+
+# also slam
+#ros2 launch my_car sim_bringup_rviz_slam.launch.xml
+
+# wall follow launch
 #ros2 launch wall_follow wall_follow_launch.xml
