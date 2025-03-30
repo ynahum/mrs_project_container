d30ef6194ccf2ab2b4cece0efb5ed8c4217463ed
Yair Nahum <ynahum@gmail.com>
2025-03-30 10:37:59 +0000

    rviz + sim only

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ set -e
 source /home/dev/ros_ws/src/setup.sh
 
 # launch AutoDRIVE Devkit with GUI and Rviz
-#ros2 launch my_car rviz_sim_bringup.launch.xml
+ros2 launch my_car rviz_sim_bringup.launch.xml
 
 # also slam
-ros2 launch my_car slam_rviz_sim_bringup.launch.xml
+#ros2 launch my_car slam_rviz_sim_bringup.launch.xml
 
 # wall follow launch
 #ros2 launch my_car wf_slam_rviz_sim_bringup.launch.xml
