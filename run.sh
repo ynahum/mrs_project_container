39bf40e5b201d4d67f2ce2227446b4cd41c8c927
Yair Nahum <ynahum@gmail.com>
2025-04-02 17:06:58 +0000

    update run script

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ set -e
 source /home/dev/ros_ws/src/setup.sh
 
 # launch nav2 and Rviz
-ros2 launch my_car nav2.launch.xml
+ros2 launch my_car nav2_rviz_bridge.launch.xml
 
 # launch AutoDRIVE Devkit with GUI and Rviz
-#ros2 launch my_car rviz_sim_bringup.launch.xml
+#ros2 launch my_car rviz_bridge.launch.xml
 
 # also slam
-#ros2 launch my_car slam_rviz_sim_bringup.launch.xml
+#ros2 launch my_car slam_rviz_bridge.launch.xml
 
 # wall follow launch
-#ros2 launch my_car wf_slam_rviz_sim_bringup.launch.xml
+#ros2 launch my_car wf.launch.xml
