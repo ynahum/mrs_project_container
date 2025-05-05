4dfd23047d685082d9bced3fda124ecd4f59e224
Yair Nahum <ynahum@gmail.com>
2025-05-05 08:18:22 +0000

    nav2_mppi_controller doesn't finish build. try other build options

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ set -e
 
 
 cd /home/dev/ros_ws/
-colcon build
-#colcon build --packages-select my_car
+#colcon build
+colcon build --packages-select my_car
+colcon build --packages-select nav2_bt_navigator
+#colcon build --packages-select nav2_mppi_controller --executor sequential  --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
 cd - > /dev/null
